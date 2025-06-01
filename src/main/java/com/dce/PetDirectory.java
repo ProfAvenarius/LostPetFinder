b1a0fa487ceb175a3fadf03a531a26fae3db68c1
DC Elliott <davecliveelliott@hotmail.com>
2025-06-01 16:25:42 -0230

    1st commit on new branch pet-directory.

---
+++ src/main/java/com/dce/PetDirectory.java
@@ -0,0 +1,9 @@
+package com.dce;
+
+import java.util.Scanner;
+
+public class PetDirectory {
+    private final PetDao petDao = new PetDao();
+    private final Scanner scanner = new Scanner(System.in);
+
+}
